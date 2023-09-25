1941ae72ebdcbbdd2a5d5653f6c7773f7c05e31f
Linux Leah <linuxleah@gmail.com>
2023-09-24 20:16:12 -0700

    Updated README. Created TODO.txt. Extended server.py to read in settings from server-settings.yaml.

--- README.md
+++ README.md
@@ -7,6 +7,15 @@ A learning AI system designed for building code.
 
 ## Installation
 
-1: Install Flask: `pip install Flask` or `pip3 install Flask`.
+1: Install prerequisites:
+* Flask: `pip install Flask` or `pip3 install Flask`.
+* PyYAML: `pip install PyYAML` or `pip3 install PyYAML`
+* If you'll be using ad-hoc HTTPS certificates (RECOMMENDED ONLY FOR TESTING/DEV!), Cryptography: `pip install cryptography` or `pip3 install cryptography`
 
+2: Edit server-settings.yaml to taste.
 
+3: Run server.py: `python3 server.py`
+
+## Troubleshooting
+
+* Getting 'Permission denied' when running the server? If server-settings.yaml does not exist, or if server-settings.yaml sets HTTPS_PORT to 443, the server would be trying to bind to the port 443, which non-root users cannot bind to. Set HTTPS_PORT to a port 1024 or greater in server-settings.yaml, or run the server as root or another superuser.
--- server.py
+++ server.py
@@ -1,4 +1,7 @@
+import os
 from flask import Flask, request
+import yaml
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -33,6 +36,35 @@ def index():
 def data():
     return "This is data from the server!"
 
+def load_config():
+    config = {'HTTPS_PORT': 443}
+    try:
+        with open('server-settings.yaml', 'r') as f:
+            loaded_config = yaml.safe_load(f)
+        config.update(loaded_config)
+    except FileNotFoundError:
+        print("settings.yaml not found, using default settings.")
+
+    certfile = config.get('SSL_CERTFILE')
+    keyfile = config.get('SSL_KEYFILE')
+
+    if certfile and keyfile:
+        if certfile and keyfile:
+            if not os.path.exists(certfile):
+                raise FileNotFoundError(f"SSL_CERTFILE {certfile} not found.")
+            if not os.path.exists(keyfile):
+                raise FileNotFoundError(f"SSL_KEYFILE {keyfile} not found.")
+
+        if not os.path.exists(certfile) or not os.path.exists(keyfile):
+            raise FileNotFoundError("SSL_CERTFILE or SSL_KEYFILE not found.")
+        config['ssl_context'] = (certfile, keyfile)
+    elif certfile or keyfile:
+        raise ValueError("Both SSL_CERTFILE and SSL_KEYFILE must be set.")
+    else:
+        config['ssl_context'] = 'adhoc'
+    return config
+
 if __name__ == "__main__":
-    app.run(ssl_context='adhoc', port=443)
+    config = load_config()
+    app.run(ssl_context=config['ssl_context'], port=config['HTTPS_PORT'])
 
